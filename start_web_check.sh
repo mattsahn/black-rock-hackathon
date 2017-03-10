d2bc0d5551ea53700760cb57e273d37583237c4f
Matt Sahn <sahn44@yahoo.com>
2017-03-10 14:23:19 +0000

    updated web check

--- start_web_check.sh
+++ start_web_check.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+cd /home/pi/Downloads/polly_poc/
 while true
 do
- node /home/pi/Downloads/polly_poc/getNewClips.js >> /home/pi/logs/clips.log
+ /home/pi/.nvm/versions/node/v6.10.0/bin/node /home/pi/Downloads/polly_poc/getNewClips.js >> /home/pi/logs/clips.log
  sleep 5
 done
